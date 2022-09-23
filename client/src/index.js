83dfd20f104198efeff08733555efb7d3a12cfaa
Ivelin Ivanov <ivelinivanov1999@gmail.com>
2022-09-23 14:31:04 +0300

    Changed to use HashRouter instead of BrowserRouter.

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { UserContextProvider } from "@Context/user/userContext";
 
